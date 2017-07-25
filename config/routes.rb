a64ca6baadf15fbe6b89dd44920b2d6e87a04da2
Nina Wilson <nina.wilson.mlis@gmail.com>
2017-07-24 20:00:08 -0400

    background

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'index' =>'welcome#index'
 
+  post 'index' => 'welcome#index'
+
   get 'test' =>'welcome#test'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
